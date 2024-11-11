5b816b1c165ad095904918295a1dfcdf4c1f5999
hws <123306796+howos1@users.noreply.github.com>
2024-11-11 03:05:12 +0300

    Add files via upload

---
+++ app/forms/MainForm.php
@@ -0,0 +1,73 @@
+<?php
+namespace app\forms;
+
+use std, gui, framework, app;
+use action\Animation; 
+
+
+class MainForm extends AbstractForm
+{
+
+    /**
+     * @event button.click-Left 
+     */
+    function doButtonClickLeft(UXMouseEvent $e = null)
+    {    
+        
+    }
+
+    /**
+     * @event label4.click-Left 
+     */
+    function doLabel4ClickLeft(UXMouseEvent $e = null)
+    {    
+        
+    }
+
+    /**
+     * @event label9.click-Left 
+     */
+    function doLabel9ClickLeft(UXMouseEvent $e = null)
+    {    
+        
+    }
+
+    /**
+     * @event buttonAlt.click-Left 
+     */
+    function doButtonAltClickLeft(UXMouseEvent $e = null)
+    {    
+        
+    }
+
+    /**
+     * @event keyDown-Shift+N 
+     */
+    function doKeyDownShiftN(UXKeyEvent $e = null)
+    {    
+        
+    }
+
+    /**
+     * @event checkbox.click-Left 
+     */
+    function doCheckboxClickLeft(UXMouseEvent $e = null)
+    {
+        
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
