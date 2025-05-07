52983c52b452b51021d8bc686adf1b89f2e3587f
hws <123306796+howos1@users.noreply.github.com>
2025-05-07 17:41:30 +0300

    Add files via upload

---
+++ Source Code/forms/addunblock.php
@@ -0,0 +1,64 @@
+<?php
+namespace app\forms;
+
+use std, gui, framework, app;
+
+
+class addunblock extends AbstractForm
+{
+
+    /**
+     * @event addsiteButton.click-Left 
+     */
+    function doAddsiteButtonClickLeft(UXMouseEvent $e = null)
+    {    
+        
+    }
+
+    /**
+     * @event closeButtonOLD.click-Left 
+     */
+    function doCloseButtonOLDClickLeft(UXMouseEvent $e = null)
+    {    
+        
+    }
+
+    /**
+     * @event gobackButton.click-Left 
+     */
+    function doGobackButtonClickLeft(UXMouseEvent $e = null)
+    {    
+        
+    }
+
+    /**
+     * @event versionLabel.click-Left 
+     */
+    function doVersionLabelClickLeft(UXMouseEvent $e = null)
+    {    
+        
+    }
+
+    /**
+     * @event closebutton.click-Left 
+     */
+    function doClosebuttonClickLeft(UXMouseEvent $e = null)
+    {    
+        
+    }
+
+    /**
+     * @event goback.click-Left 
+     */
+    function doGobackClickLeft(UXMouseEvent $e = null)
+    {    
+        
+    }
+
+
+
+
+
+
+
+}
