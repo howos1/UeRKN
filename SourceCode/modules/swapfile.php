c7a9bdef47989a93ae81ae34424b82031729e2fd
hws <123306796+howos1@users.noreply.github.com>
2025-07-14 08:43:42 +0300

    Add files via upload

---
+++ SourceCode/modules/swapfile.php
@@ -0,0 +1,35 @@
+<?php
+namespace app\modules;
+
+use std, gui, framework, app;
+
+
+class swapfile extends AbstractModule
+{
+
+    /**
+     * @event listcloudflare.action 
+     */
+    function doListcloudflareAction(ScriptEvent $e = null)
+    {    
+        
+    }
+
+    /**
+     * @event listGeneral.action 
+     */
+    function doListGeneralAction(ScriptEvent $e = null)
+    {    
+        
+    }
+
+    /**
+     * @event settings.action 
+     */
+    function doSettingsAction(ScriptEvent $e = null)
+    {    
+        
+    }
+
+
+}
